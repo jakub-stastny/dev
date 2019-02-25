fbc8161ea110b72144d7101a06c81aca7c26941f
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-02-25 17:09:26 -0500

    Add run.rb

---
+++ run.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+name = ARGV.shift || abort("Usage: #$0 project_name")
+args = Array.new
+ports = %W[3000-3005 4000-4005]
+environment = %W[DROPBOX_ACCESS_TOKEN PROWL_API_KEY]
+volumes = %W[~/.ssh ~/#{name}]
+
+ports.each do |port|
+  args << "-p #{port}:#{port}"
+end
+
+environment.each do |var_name|
+  var_content = ENV.fetch(var_name)
+  args << "-e #{var_name}=#{var_content}"
+end
+
+volumes.each do |path|
+  args << "-v #{path}:#{path}"
+end
+
+puts "docker create -it #{args.join(' ')} --name #{name} --hostname #{name} botanicus/dev"
+puts "docker start #{name}"
+
+puts "\nAutomatically connect to this docker instance on login? (Enter/C-c) "
+STDIN.read
+
+File.open(File.join(ENV['HOME'], '.bashrc'), 'a') do |file|
+  file.puts <<~EOF
+    echo "~ Connecting to the Docker instance in 1s ..."; sleep 1
+    docker attach #{name} || docker start #{name} && docker attach #{name}
+  EOF
+end
