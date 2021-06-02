fad7fc36cb6ecd28f6776bfbe52db96f04802d6b
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2021-06-02 18:48:59 +0000

    No need for Emacs on this one

--- .profile.zsh
+++ .profile.zsh
@@ -1,9 +1,6 @@
+# Let's stick to Vim for this one.
 load ~/.zsh/environments/helpers.zsh && save-function-list
-load ~/.zsh/environments/emacs.zsh
 load ~/.zsh/environments/basic.zsh
-
-start-emacs-session
-rename-first-tab
 
 # Custom functions, aliases and hooks.
 in-first-tab && git pull --rebase --autostash &> /dev/null
