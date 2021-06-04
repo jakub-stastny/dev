b58034e14831b02abb8180f54768f2e37d033289
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-04 00:50:51 +0000

    Profile

--- .profile.zsh
+++ .profile.zsh
@@ -1,10 +1,13 @@
 # Let's stick to Vim for this one.
 load ~/.zsh/environments/helpers.zsh && save-function-list
+load ~/.zsh/environments/emacs.zsh
 load ~/.zsh/environments/basic.zsh
 load ~/.zsh/environments/docker.zsh
 
 # Custom functions, aliases and hooks.
 docker-login
+start-emacs-session
+rename-first-tab
 
 push() {
   docker push jakubstastny/dev
