8ad36c00880f5720dad229d31c3e9b1acac7ebcf
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-04 02:49:02 +0000

    Moved to dotfiles

--- .profile.zsh
+++ .profile.zsh
@@ -2,6 +2,7 @@
 load ~/.zsh/environments/helpers.zsh && save-function-list
 load ~/.zsh/environments/emacs.zsh
 load ~/.zsh/environments/basic.zsh
+load ~/.zsh/environments/git.zsh
 load ~/.zsh/environments/docker.zsh
 
 # Custom functions, aliases and hooks.
@@ -11,12 +12,6 @@ rename-first-tab
 
 push() {
   docker push jakubstastny/dev
-}
-
-# TODO: open file in a Vim with a template - and pass that into the -m message.
-tag() {
-    # git tag ubuntu-21.04 -m "Ubuntu 21.04 with Emacs 27.1, NeoVim 0.4.4 and reworked dotfiles"
-    # git push --tags
 }
 
 in-first-tab && git pull --rebase --autostash &> /dev/null
