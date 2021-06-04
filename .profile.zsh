98b5fa1c484c1db8e69fa98923a57b4f182a6568
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-04 00:56:51 +0000

    tag fn

--- .profile.zsh
+++ .profile.zsh
@@ -13,6 +13,12 @@ push() {
   docker push jakubstastny/dev
 }
 
+# TODO: open file in a Vim with a template - and pass that into the -m message.
+tag() {
+    # git tag ubuntu-21.04 -m "Ubuntu 21.04 with Emacs 27.1, NeoVim 0.4.4 and reworked dotfiles"
+    # git push --tags
+}
+
 in-first-tab && git pull --rebase --autostash &> /dev/null
 in-first-tab && git install-hooks &> /dev/null
 
